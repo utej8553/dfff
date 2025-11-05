9be01d6f69490a6ab4e714782e0362bd3a4d627b
Desky <91786744+utej8553@users.noreply.github.com>
2025-11-05 19:41:07 +0530

    Update CompileCloudApplication.java

--- src/main/java/org/utej/compilecloud/CompileCloudApplication.java
+++ src/main/java/org/utej/compilecloud/CompileCloudApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompileCloudApplication {
     static {
-        // Prefer modern Windows ConPTY over winpty (less Control-C weirdness)
         System.setProperty("pty4j.preferConpty", "true");
-        // Optional: be stricter about console closing behavior
         System.setProperty("winp.useLegacyConPty", "false");
     }
 
